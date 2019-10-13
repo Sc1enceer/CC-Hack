82206aaceee15cbbacec6271b3a8efb4bfc84786
Dennison Lau <laudennison@hotmail.com>
2019-10-13 10:52:54 +0000

    cchack version 2

--- pi-zero_hardware/cchack_v1.py
+++ pi-zero_hardware/cchack_v1.py
@@ -2,7 +2,12 @@ from ltr559 import LTR559
 from as7262 import AS7262
 import requests
 import time
+import os
+import sys
+import signal
+import VL53L1X
 
+#setting up database communication
 ts= time.time()
 
 SenInput=[]
@@ -24,7 +29,7 @@ def getDataForSpectrum():
     for type, value, timestamp in SenInput:
         data+= "spectrum" +",type=spectrum " +type+"="+str(value)+" "+timestamp +"\n"		    
         if SenInput==SenInput[-1]:
-            data+= "spectrum" +",type=spectrum " +type+ "="+str(value)+" "+timestamp
+            data+= "spectrum" +",type=spectrum " +type+"="+str(value)+" "+timestamp
     return data
 	
 def send():
@@ -42,20 +47,44 @@ def sendSpectrum():
 
 #setting up the light sensor
 ltr559 = LTR559()
+#setting up time of flight sensor
+tof = VL53L1X.VL53L1X(i2c_bus = 1,i2c_address=0x29)
+tof.open()
+tof.start_ranging(2)
 #setting up the spectral sensor
 as7262 = AS7262()
-
 as7262.set_gain(64)
 as7262.set_integration_time(17.857)
 as7262.set_measurement_mode(2)
 as7262.set_illumination_led(0)
+#listing out useful algorithm
+#Modified Chlorophyll Absorption Ratio Index => indicates the relative abundance of chlorophyll 
+def MCARI(red,orange,green):
+	result = ((red - orange) - 0.2*(red - green))*(red/orange)
+	return result
+#Carotenoid Reflectance Index 1 (CRI1) => function in light absorption processes in plants. Weakening vegetation contains higher conc. of carotenoids,measure of stressed vegetation. High CRI1 mean greater conc. than chlorophyll
+def CRI1 (blue,green):
+	result = ((1/blue)-(1/green))
+	return result
+#Photochemical Reflectance Index (PRI) => reflectance measurement that is sensitive to changes in caretenoid pigments in live foliage, indicator of photosynthetic light use efficiency, or rate of CO2 uptake, study vegetaion productivity abd stress which includes vegetation health
+def PRI (orange,green):
+	result = (green - orange)/(orange + green)
+	return result
+
 
 try:
     while True:
         lux = ltr559.get_lux();
-        if(lux < 150):
+        distance = tof.get_distance()/10
+        appendtosend("ToF",distance)
+        if(lux < 100):
             as7262.set_illumination_led(1)
             values = as7262.get_calibrated_values()
+            average = (values.red + values.orange + values.yellow + values.green + values.blue + values.violet)/6
+            appendtosend("MCARI",MCARI(values.red,values.orange,values.green))
+            appendtosend("CRI1",CRI1(values.blue,values.green))
+            appendtosend("PRI",PRI(values.orange,values.green))
+            appendtosend("AverageSpectral",average)
             appendtosend("Lux",lux)
             appendtosend("Red",values.red)
             appendtosend("Orange",values.orange)
@@ -72,6 +101,11 @@ try:
         else:
             as7262.set_illumination_led(0)
             values = as7262.get_calibrated_values()
+            average = (values.red + values.orange + values.yellow + values.green + values.blue + values.violet)/6
+            appendtosend("MCARI",MCARI(values.red,values.orange,values.green))
+            appendtosend("CRI1",CRI1(values.blue,values.green))
+            appendtosend("PRI",PRI(values.orange,values.green))            
+            appendtosend("AverageSpectral",average)
             appendtosend("Lux",lux)
             appendtosend("Red",values.red)
             appendtosend("Orange",values.orange)
@@ -90,3 +124,4 @@ try:
 except KeyboardInterrupt:
     as7262.set_measurement_mode(3)
     as7262.set_illumination_led(0)
+    tof.stop_ranging()
